8a8edc8a723eeb8c43f0afcb97494d40fb6769fe
NOH DOYEON <109110721+hyedal95@users.noreply.github.com>
2024-02-08 18:14:37 +0900

    0208 안산그린컴퓨터아카데미 산출물

---
+++ 풀스택_4기_노도연/강의산출물/0208_01_산출물_JUST쇼핑몰_웹제작_와이어프레임_한번 더하기/script/script.js
@@ -0,0 +1,5 @@
+(function($){
+  /*   alert('제이쿼리');
+    console.log($);
+ */
+})(jQuery);
---
+++ 풀스택_4기_노도연/강의산출물/0208_04_산출물_JUST쇼핑몰_웹제작_와이어프레임_네비게이션_메인슬라이드_제이쿼리/script/script.js
@@ -0,0 +1,55 @@
+(function($){
+    const just ={
+        init(){
+            this.header();
+            this.section1();
+            this.section2();
+        },
+
+        header(){
+            //메인버튼에 마우스 오버시(mouseenter ===mouseover) 서브메뉴 박스 전체가 아래로
+            //부드럽게 나타난다. 슬라이드 다운 애니메이션 이벤트 구현
+            $('.main-btn').on({
+                mouseenter(){
+                    $('.sub').stop().slideDown(300);
+                }
+            });
+
+            //서브메뉴를 벗어나면 부드럽게 다시 올라간다. (mouseleave ===)
+            $('.sub').on({
+                mouseleave(){
+                    $('.sub').stop().slideUp(300);
+                }
+            });
+        },
+        section1(){
+            //1.변수
+            let cnt=0;
+            let setId=0;
+            //2.메인슬라이드
+            function mainSlide(){ /* 0~2 */
+                $('.slide-wrap').stop().animate({top:`${-300*cnt}px`},600,function(){
+                    if(cnt>2) cnt=0;
+                    $('.slide-wrap').stop().animate({top:`${-300*cnt}px`},0)
+                });
+            }
+            //3.다음카운트
+            function nextCount(){
+                cnt+=1;
+                mainSlide();
+            }
+            //4.자동타이머 3초 간격
+            function autoTimer(){
+                setId=setInterval(nextCount,3000);
+
+            }autoTimer();
+
+        },
+        section2(){
+
+        },
+
+    }
+    just.init();
+
+})(jQuery);
